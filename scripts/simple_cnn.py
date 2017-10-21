60df6c8f6331459f4fbd72156ac29628227fd0ac
k123321141 <kk123321141@gamil.com>
2017-10-22 05:02:50 +0800

    quickly

--- scripts/simple_cnn.py
+++ scripts/simple_cnn.py
@@ -27,15 +27,15 @@ first_input = Input(shape=(max_len,features_count,1))
 '''
 xx = Conv2D(30,input_shape = (max_len,features_count),kernel_size = (1,features_count),padding='valid',activation = 'relu',data_format = 'channels_last')(first_input) 
 '''
-xx = Conv2D(20,input_shape = (max_len,features_count),kernel_size = (1,5),padding='valid',activation = 'relu',data_format = 'channels_last')(first_input) 
+xx = Conv2D(30,input_shape = (max_len,features_count),kernel_size = (1,5),padding='valid',activation = 'relu',data_format = 'channels_last')(first_input) 
 
 #777,35,30
-xx = Conv2D(40,kernel_size = (1,5),padding='valid',activation = 'relu',data_format = 'channels_last')(xx) 
-xx = Conv2D(80,kernel_size = (1,31),padding='valid',activation = 'relu',data_format = 'channels_last')(xx)
+xx = Conv2D(60,kernel_size = (1,5),padding='valid',activation = 'relu',data_format = 'channels_last')(xx) 
+xx = Conv2D(90,kernel_size = (1,31),padding='valid',activation = 'relu',data_format = 'channels_last')(xx)
 #(777,1,120)
 #xx = Conv2D(30,kernel_size = (1,3),padding='valid',activation = 'relu',data_format = 'channels_last')(xx)
 #(777,35,30)
-xx = Reshape((max_len,80))(xx)
+xx = Reshape((max_len,90))(xx)
 xx = Masking(mask_value=0)(xx)
 rnn_lay = GRU
 xx = rnn_lay(60,activation='tanh',return_sequences=True,implementation=1)(xx)
@@ -51,6 +51,6 @@ plot_model(model, to_file='../model.png',show_shapes = True)
 model.compile(loss='categorical_crossentropy', optimizer='rmsprop',metrics=['accuracy'])
 early_stopping = EarlyStopping(monitor='val_loss', patience=2)
 
-model.fit(x,y,batch_size = 100,epochs = 200,callbacks=[early_stopping],validation_split = 0.05)
+model.fit(x,y,batch_size = 10,epochs = 200,callbacks=[early_stopping],validation_split = 0.05)
 
 model.save('../models/simple_k.model')
