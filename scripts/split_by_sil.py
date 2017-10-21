633c30b7b47c73064d683cbdfceb7bd9a39af3a4
k123321141 <kk123321141@gamil.com>
2017-10-21 18:04:04 +0800

    quickly

--- scripts/split_by_sil.py
+++ scripts/split_by_sil.py
@@ -1,11 +1,6 @@
 import myinput
 from keras.models import *
-from keras.layers import *
-from keras.utils import *
-from keras.callbacks import EarlyStopping
-from keras.optimizers import *
 import numpy as np
-import random
 import dic_processing
 
 num_classes = 48
@@ -16,40 +11,50 @@ max_len = 777
 dic1 = myinput.load_test('../data/mfcc/test.ark')
 x,fake_y = dic_processing.toXY(dic1)
 
-dic1 = myinput.load_input('mfcc')
-#
-dic_processing.pad_dic(dic1,max_len,max_len,0)
-dic_processing.catogorate_dic(dic1,2)
+sample_num,sample_len,features_count = x.shape
+
+assert max_len == sample_len 
+
+x = x.reshape(sample_num,max_len,features_count,1)
+
+model = load_model('../models/sil.model')
+
+y = model.predict(x)
+
+keys = sorted(dic1.keys())
+total = len(keys)
+
+def split_test(y_seq):
+    pre = y_seq[0]
+    s = ''
+    c = 1 if pre == 0 else 0
+    for y in y_seq[1:]:
+        if y == 1 and pre == 1:
+            pre = y
+        elif pre == 1 and y == 0:
+            pre = y
+            c = 1
+        elif pre == 0 and y == 1:
+            s += str(c)
+            pre = y
+        elif pre == 0 and y == 0:
+            c += 1
+            pre = y
+        else:
+            print 'error'
+    return s
+
+id = 'maeb0_si1411'
+index = keys.index(id)
+buf = y[index,:,:]
+frame_seq = [argmax(buf[j,:]) for j in range(max_len)]
+print split_test(frame_seq)
 
 
+'''
+for i in range(total):
+    sentenceID = keys[i]
+    buf = y[i,:,:]
+    frame_seq = [argmax(buf[j,:]) for j in range(max_len)]
 
-x,y = dic_processing.toXY(dic1)
-sample_num,max_len_sample,features_count = x.shape
-assert max_len == max_len_sample
-
-
-rnn_lay = SimpleRNN
-model = Sequential()
-#CNN
-x = x.reshape(sample_num,max_len_sample,features_count,1)
-#model.add(Reshape( (max_len,features_count,1),input_shape = (max_len,features_count)))
-k = 7
-f_c = 15
-model.add(Conv2D(f_c,kernel_size = (1,7),activation = 'relu',input_shape = (max_len,features_count,1)))
-
-model.add(Conv2D(f_c*2,kernel_size = (1,features_count-k+1),activation = 'relu'))
-model.add(Reshape( (max_len,f_c*2)))
-#rnn
-model.add(Masking(mask_value=0))
-#model.add(rnn_lay(30,input_dim = features_count, activation='tanh',return_sequences=True,implementation=1))
-model.add(rnn_lay(30,activation='tanh',return_sequences=True,implementation=1))
-model.add(rnn_lay(10, activation='tanh',return_sequences=True,implementation=1))
-model.add(TimeDistributed(Dense(2,activation='sigmoid')))
-
-opt = SGD(lr = 0.001)
-model.compile(optimizer=opt,loss='categorical_crossentropy',metrics=['accuracy'])
-early_stopping = EarlyStopping(monitor='val_loss', patience=2)
-
-model.fit(x,y,batch_size = 10,epochs = 200,callbacks=[early_stopping],validation_split = 0.05)
-
-model.save('../models/sil.model')
+'''
