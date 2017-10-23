4638b96ae3546cdb6e95c0237cbf3ac9ad0313a2
k123321141 <kk123321141@gamil.com>
2017-10-23 15:02:17 +0800

    quickly

--- scripts/cnn.py
+++ scripts/cnn.py
@@ -30,23 +30,20 @@ first_input = Input(shape=(max_len,features_count,1))
 xx = Conv2D(30,input_shape = (max_len,features_count),kernel_size = (1,features_count),padding='valid',activation = 'relu',data_format = 'channels_last')(first_input) 
 '''
 xx = Conv2D(30,input_shape = (max_len,features_count),kernel_size = (1,5),padding='valid',activation = 'relu',data_format = 'channels_last')(first_input) 
-xx = BatchNormalization(axis=-1 )(xx)
 #777,35,30
 xx = Conv2D(60,kernel_size = (1,5),padding='valid',activation = 'relu',data_format = 'channels_last')(xx) 
+#777,31,60
+#xx = Conv2D(90,kernel_size = (1,31),padding='valid',activation = 'relu',data_format = 'channels_last')(xx)
 xx = BatchNormalization(axis=-1 )(xx)
-xx = Conv2D(90,kernel_size = (1,31),padding='valid',activation = 'relu',data_format = 'channels_last')(xx)
-xx = BatchNormalization(axis=-1 )(xx)
-#(777,1,120)
-#xx = Conv2D(30,kernel_size = (1,3),padding='valid',activation = 'relu',data_format = 'channels_last')(xx)
-#(777,35,30)
-xx = Reshape((max_len,90))(xx)
+xx = Reshape((max_len,31*60))(xx)
 xx = Masking(mask_value=0)(xx)
 if bi == True:
 
     xx = Bidirectional(rnn_lay(60,activation='tanh',return_sequences=True,implementation=1))(xx)
-    xx = Bidirectional(rnn_lay(40,activation='tanh',return_sequences=True,implementation=1))(xx)
+#    xx = Bidirectional(rnn_lay(40,activation='tanh',return_sequences=True,implementation=1))(xx)
     xx = Bidirectional(rnn_lay(num_classes+1,activation='tanh',return_sequences=True,implementation=1))(xx)
-    xx = rnn_lay(num_classes+1,activation='softmax',return_sequences=True,implementation=1)(xx)
+    xx = TimeDistributed(Dense(num_classes+1,activation = 'softmax'))(xx)
+#    xx = rnn_lay(num_classes+1,activation='softmax',return_sequences=True,implementation=1)(xx)
 else:
     xx = Bidirectional(rnn_lay(60,activation='tanh',return_sequences=True,implementation=1))(xx)
     xx = rnn_lay(40,activation='tanh',return_sequences=True,implementation=1)(xx)
