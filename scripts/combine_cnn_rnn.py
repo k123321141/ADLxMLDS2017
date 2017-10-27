2ae9776d4bb1d1be1b0889c5bd1be5c810d3dc70
k123321141 <kk123321141@gamil.com>
2017-10-27 10:51:49 +0800

    quickly

--- scripts/combine_cnn_rnn.py
+++ scripts/combine_cnn_rnn.py
@@ -2,8 +2,36 @@ from keras.models import *
 from keras.layers import *
 from keras.utils import to_categorical
 import keras.backend as K
-def ctc_lambda_func(y_true, y_pred):
-    return K.ctc_batch_cost(y_true, y_pred, 777, 80)
+import tensorflow as tf
+c = K.constant([48]*773,tf.int64)
+def acc_with_mask(y_true, y_pred):
+    mask = K.not_equal(K.argmax(y_true,-1),c)
+    correct = K.equal(K.argmax(y_true,-1),K.argmax(y_pred,-1))
+    #3696,777 
+    mask = tf.cast(mask,tf.float32)
+    correct = tf.cast(correct,tf.float32)
+    correct = tf.reduce_sum(mask * correct)
+
+    
+    return (correct) / tf.reduce_sum(mask)
+
+def cross_entropy(y_true, y_pred):
+    return tf.nn.softmax_cross_entropy_with_logits(labels = y_true,logits = y_pred)
+def my_cross_entropy(y_true, y_pred):
+    cross_entropy = y_true * tf.log(y_pred)
+
+    cross_entropy = -tf.reduce_sum(cross_entropy,axis=-1)
+    return tf.reduce_mean(cross_entropy)
+def loss_with_mask(y_true, y_pred):
+    mask = K.not_equal(K.argmax(y_true,-1),c)
+    mask = tf.cast(mask,tf.float32)
+
+    cross_entropy = y_true * tf.log(y_pred)
+    cross_entropy = -tf.reduce_sum(cross_entropy,axis=-1)
+    
+    cross_entropy = cross_entropy * mask
+    return tf.reduce_mean(cross_entropy) 
+    #return cross_entropy
 
 if __name__ == '__main__':
     import myinput
@@ -26,10 +54,14 @@ if __name__ == '__main__':
     x = x.reshape(num,max_len,features_count,1)
     first_input = Input(shape=(max_len,features_count,1))
     cnn_input = BatchNormalization(axis = -1)(first_input)
-    cnn_output = Conv2D(30,kernel_size = (3,5),use_bias = False,activation = 'relu',padding = 'valid')(cnn_input)
-    #(87,39,1) -> (85,35,10)
-    seq_input = Reshape((max_len-2,35*30))(cnn_output)
+    cnn_output = Conv2D(10,kernel_size = (3,5),use_bias = False,activation = 'relu',padding = 'valid')(cnn_input)
+    #(777,39,1) -> (777,35,10)
+    cnn_output = Conv2D(30,kernel_size = (3,5),use_bias = False,activation = 'relu',padding = 'valid')(cnn_output)
+    #(777,35,10) -> (773,31,30)
+    print cnn_output.shape
+    seq_input = Reshape((max_len-4,31*30))(cnn_output)
     seq_input = Masking()(seq_input)
+    #seq_input= Dropout(0.5)(seq_input)
     #
     rnn_lay = LSTM
 
@@ -38,11 +70,12 @@ if __name__ == '__main__':
     x1,state_h, state_c  = rnn_lay(300,activation = 'tanh',return_state = True,return_sequences = True)(seq_input)
     x2,state_h, state_c  = rnn_lay(300,activation = 'tanh',return_state = True,return_sequences = True,go_backwards = True)(seq_input,[state_h, state_c])
     xx = Concatenate(axis = -1)([x1,x2])
+    xx = Dropout(0.5)(xx)
     x1,state_h, state_c  = rnn_lay(300,activation = 'tanh',return_state = True,return_sequences = True)(xx)
     x2 = rnn_lay(300,activation = 'tanh',return_state = False,return_sequences = True,go_backwards = True)(xx,[state_h, state_c])
     xx = Concatenate(axis = -1)([x1,x2])
     
-    xx = Dropout(0.25)(xx)
+    xx = Dropout(0.5)(xx)
     result = TimeDistributed(Dense(num_classes+1,activation='softmax'))(xx)
 
     model = Model(input = first_input,output = result)
@@ -50,24 +83,22 @@ if __name__ == '__main__':
     #model.load_weights('../checkpoints/simple.18-1.65.model')
 
     #reduce y
-    y = y[:,1:-1,:]
+    y = y[:,2:-2,:]
     s_mat = np.ones(y.shape[0:2],dtype = np.float32)
     for i in range(y.shape[0]):
             for j in range(y.shape[1]):
                 if y[i,j,-1] == 1:
-                    end = np.min([j+4,y.shape[1]])
-                    s_mat[i,j+1:end] = 1
-                    s_mat[i,j] = 5
-                    s_mat[i,end:] = 0
+                    #s_mat[i,j] = 5
+                    s_mat[i,j:] = 0
                     break
                 elif y[i,j,37] == 1: #sil
                     s_mat[i,j] = 0.5
 
     opt = Adam(lr = 0.001)
     model.compile(loss='categorical_crossentropy', optimizer=opt,metrics=['accuracy'],sample_weight_mode = 'temporal')
+    model.compile(loss=loss_with_mask, optimizer=opt,metrics=[acc_with_mask],sample_weight_mode = 'temporal')
     early_stopping = EarlyStopping(monitor='val_loss', patience=3)
-    cks = ModelCheckpoint('../checkpoints/com.{epoch:02d}-{val_loss:.2f}.cks',save_best_only=True,period = 2)
+    cks = ModelCheckpoint('../checkpoints/comwithmask.{epoch:02d}-{val_loss:.2f}.cks',save_best_only=True,period = 2)
 
     model.fit(x,y,batch_size = 30,epochs = 2000,callbacks=[early_stopping,cks],validation_split = 0.05,sample_weight = s_mat)
-    #model.fit(x,y,batch_size = 30,epochs = 2000,callbacks=[early_stopping,cks],validation_split = 0.05)
     print 'Done'
--- scripts/seq2seq.py
+++ scripts/seq2seq.py
@@ -2,8 +2,36 @@ from keras.models import *
 from keras.layers import *
 from keras.utils import to_categorical
 import keras.backend as K
-def ctc_lambda_func(y_true, y_pred):
-    return K.ctc_batch_cost(y_true, y_pred, 777, 80)
+import tensorflow as tf
+c = K.constant([49]*80,tf.int64)
+def acc_with_mask(y_true, y_pred):
+    mask = K.not_equal(K.argmax(y_true,-1),c)
+    correct = K.equal(K.argmax(y_true,-1),K.argmax(y_pred,-1))
+    #3696,777 
+    mask = tf.cast(mask,tf.float32)
+    correct = tf.cast(correct,tf.float32)
+    correct = tf.reduce_sum(mask * correct)
+
+    
+    return (correct) / tf.reduce_sum(mask)
+
+def cross_entropy(y_true, y_pred):
+    return tf.nn.softmax_cross_entropy_with_logits(labels = y_true,logits = y_pred)
+def my_cross_entropy(y_true, y_pred):
+    cross_entropy = y_true * tf.log(y_pred)
+
+    cross_entropy = -tf.reduce_sum(cross_entropy,axis=-1)
+    return tf.reduce_mean(cross_entropy)
+def loss_with_mask(y_true, y_pred):
+    mask = K.not_equal(K.argmax(y_true,-1),c)
+    mask = tf.cast(mask,tf.float32)
+
+    cross_entropy = y_true * tf.log(y_pred)
+    cross_entropy = -tf.reduce_sum(cross_entropy,axis=-1)
+    
+    cross_entropy = cross_entropy * mask
+    return tf.reduce_mean(cross_entropy) 
+    #return cross_entropy
 
 if __name__ == '__main__':
     import myinput
@@ -18,8 +46,6 @@ if __name__ == '__main__':
     max_out_len = 80
     seq = True
     dic1 = myinput.load_input('mfcc')
-    buf_x = []
-    buf_y = []
     if seq == True:
         seq_dict = myinput.read_seq_Y('../data//mfcc/seq_y.lab')
         for sentenceID in sorted(seq_dict.keys()):
@@ -29,63 +55,65 @@ if __name__ == '__main__':
 
             x,y = dic1[sentenceID]
             dic1[sentenceID] = x,seq_y
-        dic_processing.pad_dic(dic1,max_len,max_out_len,num_classes)
+        dic_processing.pad_dic(dic1,max_len,max_out_len,49) #padding
     else:
        for sentenceID in sorted(dic1.keys()):
             x,y = dic1[sentenceID]
-            buf_x.append(x)
-            buf_y.append(y)
-    dic_processing.catogorate_dic(dic1,num_classes+1)
+    dic_processing.catogorate_dic(dic1,num_classes+2)
     x,y = dic_processing.toXY(dic1)
     num = x.shape[0]
     
-    print x.shape
+    #model setting
+    
     x = x.reshape(num,max_len,features_count,1)
     first_input = Input(shape=(max_len,features_count,1))
     cnn_input = BatchNormalization(axis = -1)(first_input)
-    cnn_output = Conv2D(30,kernel_size = (3,5),use_bias = False,activation = 'relu',padding = 'valid')(cnn_input)
-    #(87,39,1) -> (85,35,10)
-    seq_input = Reshape((max_len-2,35*30))(cnn_output)
+    cnn_output = Conv2D(10,kernel_size = (1,5),use_bias = False,activation = 'relu',padding = 'valid')(cnn_input)
+    #(777,39,1) -> (777,35,10)
+    cnn_output = Dropout(0.5)(cnn_output)
+    cnn_output = BatchNormalization(axis = -1)(cnn_output)
+    cnn_output = Conv2D(30,kernel_size = (3,7),use_bias = False,activation = 'relu',padding = 'valid')(cnn_output)
+    cnn_output = Dropout(0.5)(cnn_output)
+    #(777,35,10) -> (775,29,30)
+    cnn_output = BatchNormalization(axis = -1)(cnn_output)
+    seq_input = Reshape((max_len-2,29*30))(cnn_output)
     seq_input = Masking()(seq_input)
     #
     rnn_lay = LSTM
-
-    #xx,state_h, state_c = (rnn_lay(300,activation = 'tanh',return_state = True))(seq_input)
-    #bidirection
-    x1,state_h, state_c  = rnn_lay(300,activation = 'tanh',return_state = True)(seq_input)
-    x2,state_h, state_c  = rnn_lay(300,activation = 'tanh',return_state = True,go_backwards = True)(seq_input,[state_h, state_c])
+    #auto decoder encoder
+    x1,state_h, state_c  = rnn_lay(300,activation = 'tanh',recurrent_dropout = 0.5,return_state = True)(seq_input)
+    x2,state_h, state_c  = rnn_lay(300,activation = 'tanh',recurrent_dropout = 0.5,return_state = True,go_backwards = True)(seq_input,[state_h, state_c])
     xx = Concatenate(axis = -1)([x1,x2])
 
     xx = RepeatVector(max_out_len)(xx)
-    x1,state_h, state_c  = rnn_lay(300,activation = 'tanh',return_state = True,return_sequences = True)(xx,[state_h, state_c])
-    x2 = rnn_lay(300,activation = 'tanh',return_state = False,return_sequences = True,go_backwards = True)(xx,[state_h, state_c])
+    x1,state_h, state_c  = rnn_lay(300,activation = 'tanh',return_state = True,recurrent_dropout = 0.5,return_sequences = True)(xx,[state_h, state_c])
+    x2 = rnn_lay(300,activation = 'tanh',return_state = False,return_sequences = True,recurrent_dropout = 0.5,go_backwards = True)(xx,[state_h, state_c])
     xx = Concatenate(axis = -1)([x1,x2])
     
-    xx = Dropout(0.25)(xx)
-    result = TimeDistributed(Dense(num_classes+1,activation='softmax'))(xx)
+    xx = Dropout(0.5)(xx)
+    result = TimeDistributed(Dense(50,activation='softmax'))(xx)
 
     model = Model(input = first_input,output = result)
     plot_model(model, to_file='../model.png',show_shapes = True)
-    model.load_weights('../checkpoints/seq.05-1.49.cks')
-
+#    model.load_weights('../checkpoints/seq.09-1.46.cks')
+    
+    
     s_mat = np.ones(y.shape[0:2],dtype = np.float32)
-    for i in range(y.shape[0]):
-            for j in range(y.shape[1]):
+    for i in range(y.shape[0]): #3696
+            for j in range(y.shape[1]):#777
                 if y[i,j,-1] == 1:
-                    end = np.min([j+4,y.shape[1]])
-                    s_mat[i,j+1:end] = 1
+                    y[i,j,-1] = 0
+                    y[i,j,-2] = 1   #end of sentence 
+                    s_mat[i,j:] = 0
                     s_mat[i,j] = 5
-                    s_mat[i,end:] = 0
                     break
                 elif y[i,j,37] == 1: #sil
                     s_mat[i,j] = 0.5
 
     opt = Adam(lr = 0.001)
-    model.compile(loss='categorical_crossentropy', optimizer=opt,metrics=['accuracy'],sample_weight_mode = 'temporal')
-    #model.compile(loss=ctc_lambda_func, optimizer=sgd_opt,metrics=['accuracy'],sample_weight_mode = 'temporal')
-    early_stopping = EarlyStopping(monitor='val_loss', patience=3)
+    model.compile(loss=loss_with_mask, optimizer=opt,metrics=[acc_with_mask],sample_weight_mode = 'temporal')
+    early_stopping = EarlyStopping(monitor='val_loss', patience=10)
     cks = ModelCheckpoint('../checkpoints/seq.{epoch:02d}-{val_loss:.2f}.cks',save_best_only=True,period = 2)
 
     model.fit(x,y,batch_size = 30,epochs = 2000,callbacks=[early_stopping,cks],validation_split = 0.05,sample_weight = s_mat)
-    #model.fit(x,y,batch_size = 30,epochs = 2000,callbacks=[early_stopping,cks],validation_split = 0.05)
     print 'Done'
