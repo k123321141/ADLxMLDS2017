26b6888d11c19a7c66da5ecfdcaaa9dc3629844b
k123321141 <kk123321141@gamil.com>
2017-11-15 13:10:06 +0800

    quickly

--- hw2/scripts/evaluate.py
+++ hw2/scripts/evaluate.py
@@ -2,11 +2,12 @@
 from keras.callbacks import *
 import keras
 import numpy as np
-import my_model
+import seq2seq
 import myinput
 import sys
 from HW2_config import *
 from keras.models import load_model
+import utils
 from myinput import decode,batch_decode
 if __name__ == '__main__':
     assert len(sys.argv) == 3
@@ -15,7 +16,7 @@ if __name__ == '__main__':
     print('load testing data.')
     test_dic = myinput.load_x_dic('../data/testing_data/feat/')
     print('load model.')
-    model = load_model(model_path,custom_objects={'loss_with_mask':my_model.loss_with_mask,'acc_with_mask':my_model.acc_with_mask})
+    model = load_model(model_path,custom_objects={'loss_with_mask':utils.loss_with_mask,'acc_with_mask':utils.acc_with_mask})
     #
     print('init decode map')
     vocab_map = myinput.init_vocabulary_map()
@@ -27,7 +28,7 @@ if __name__ == '__main__':
         buf_x = np.zeros([num,input_len,input_dim],dtype=np.float32)
         for i,k in enumerate(sorted(test_dic.keys())):
             buf_x[i,:,:] = test_dic[k]
-        preds = my_model.batch_pred(model,buf_x,output_len)
+        preds = seq2seq.batch_pred(model,buf_x,output_len)
 
         guess = batch_decode(decode_map,preds)
         for i,k in enumerate(sorted(test_dic.keys())):
