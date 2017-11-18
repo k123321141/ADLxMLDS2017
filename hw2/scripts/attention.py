7d90394d92054278afc7e7e25afc1b6fd2ebdbfb
k123321141 <kk123321141@gamil.com>
2017-11-18 15:47:39 +0800

    quickly

--- hw2/scripts/attention.py
+++ hw2/scripts/attention.py
@@ -33,7 +33,8 @@ def model(input_len,input_dim,output_len,vocab_dim):
         x = Concatenate(axis = -1)([ret1[0],ret2[0]])
         #prepare hidden state for encoder
         hi_st = ret2[1:] if config.RNN == LSTM else ret2[1]
-        x = Dropout(config.DROPOUT)(x)
+        if _ != config.DEPTH -1 :
+            x = Dropout(config.DROPOUT)(x)
 
     #word embedding
     y = Dense(config.EMBEDDING_DIM,activation = 'linear',use_bias = False)(y)
