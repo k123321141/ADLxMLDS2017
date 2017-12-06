3aeb7772abed170ecc6de5d92489ecd0d8a5d295
k123321141 <kk123321141@gamil.com>
2017-12-06 16:34:50 +0800

    quickly

--- hw3/agent_dir/agent_dqn.py
+++ hw3/agent_dir/agent_dqn.py
@@ -1,4 +1,5 @@
 from agent_dir.agent import Agent
+import tensorflow as tf
 
 class Agent_DQN(Agent):
     def __init__(self, env, args):
@@ -12,7 +13,7 @@ class Agent_DQN(Agent):
         if args.test_dqn:
             #you can load your model here
             print('loading trained model')
-
+        print('init')
         ##################
         # YOUR CODE HERE #
         ##################
@@ -28,7 +29,9 @@ class Agent_DQN(Agent):
         ##################
         # YOUR CODE HERE #
         ##################
+        print('init game 1')
         pass
+        print('init game 2')
 
 
     def train(self):
@@ -38,8 +41,28 @@ class Agent_DQN(Agent):
         ##################
         # YOUR CODE HERE #
         ##################
+        print('init train 1')
         pass
+        print('init train 2')
 
+    def build_network(self):
+        model = Sequential()
+        model.add(Conv2D(32, kernel_size = (8,8), activation='relu', input_shape=(210, 160, 3), data_format = 'channels_last'))
+        model.add(MaxPooling2D((2,2) ))
+        model.add(Conv2D(64, kernel_size = (4,4), activation='relu'))
+        model.add(MaxPooling2D((2,2) ))
+        model.add(Conv2D(64, kernel_size = (3,2), activation='relu'))
+        model.add(MaxPooling2D((2,2) ))
+        model.add(Conv2D(32, kernel_size = (8,8), activation='relu'))
+        
+        model.add(Flatten())
+        model.add(Dense(512, activation='relu'))
+        model.add(Dense(4, activation='linear'))
+
+        s = tf.placeholder(tf.float32, [None, 210, 160, 3])
+        q_values = model(s)
+
+        return s, q_values, model
 
     def make_action(self, observation, test=True):
         """
@@ -56,5 +79,6 @@ class Agent_DQN(Agent):
         ##################
         # YOUR CODE HERE #
         ##################
+        print('make action')
         return self.env.get_random_action()
 
