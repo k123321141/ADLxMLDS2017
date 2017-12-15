98605982debcab304d2eb46af12b57c2c99d009b
k123321141 <kk123321141@gamil.com>
2017-12-15 21:43:44 +0800

    rm bash

--- hw3/bash.sh
+++ hw3/bash.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
+: '
 python3 ./main.py --train_dqn \
     --dqn_model=/home/k123/rl/double_DQN.h5 \
     --dqn_summary=/home/k123/rl/double_DQN_summary \
     --dqn_double_dqn \
     --dqn_max_spisode=5000
-
+'
 python3 ./main.py --train_dqn \
-    --dqn_model=/home/k123/rl/duel_DQN.h5 \
-    --dqn_summary=/home/k123/rl/duel_DQN_summary \
+    --dqn_model=./rl/duel_DQN.h5 \
+    --dqn_summary=./rl/duel_DQN_summary \
     --dqn_max_spisode=5000
     --dqn_dueling
 python3 ./main.py --train_dqn \
-    --dqn_model=/home/k123/rl/DQN.h5 \
-    --dqn_summary=/home/k123/rl/DQN_summary \
-    --dqn_max_spisode=500000
+    --dqn_model=./rl/DQN.h5 \
+    --dqn_summary=./rl/DQN_summary \
+    --dqn_max_spisode=5000
 #CUDA_VISIBLE_DEVICES="" python3 ./main.py --train_dqn --dqn_model=/tmp/rl/123.h5 --dqn_epsilon=0.5 --dqn_double_dqn #--dqn_dueling
 #CUDA_VISIBLE_DEVICES="" python3 ./main.py --train_dqn --dqn_model=/tmp/rl/123.h5 --dqn_epsilon=0.5 --dqn_double_dqn #--dqn_dueling
--- hw3/bash_main.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-CUDA_VISIBLE_DEVICES="" python3 ./main.py --train_pg
