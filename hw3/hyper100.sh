b0d58f28f6946c509371ceaa7a563aa601ad59cd
k123321141 <kk123321141@gamil.com>
2017-12-16 17:48:26 +0800

    quickly

--- hw3/hyper100.sh
+++ hw3/hyper100.sh
@@ -2,5 +2,6 @@
 CUDA_VISIBLE_DEVICES="" python3 ./main.py --train_dqn \
     --dqn_model=/home/k123/rl/DQN_update_target100.h5 \
     --dqn_summary=/home/k123/rl/DQN_update_target100_summary \
+    --dqn_update_target=100 \
     --dqn_max_spisode=5000 \
-    --dqn_memory=5000 
+    --dqn_memory=3000 
