6e17c468a838c91f5f320b460889d63b7792e6f0
k123321141 <kk123321141@gamil.com>
2017-12-16 14:50:05 +0800

    quickly

--- hw3/tmp.sh
+++ hw3/tmp.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 python3 ./main.py --train_dqn --dqn_dueling \
     --dqn_model=./rl/DQN_duel.h5 \
-    --dqn_summary=./rl/summary/DQN_summary \
-    --dqn_max_spisode=5000
+    --dqn_summary=./rl/summary/DQN_duel_summary \
+    --dqn_max_spisode=5000 \
     --dqn_memory=30000 
 python3 ./main.py --train_dqn \
     --dqn_model=./rl/DQN_update_target100000.h5 \
-    --dqn_summary=./rl/DQN_update_target100000_summary \
+    --dqn_summary=./rl/isummary/QN_update_target100000_summary \
     --dqn_max_spisode=5000 \
     --dqn_memory=30000 
 #CUDA_VISIBLE_DEVICES="" python3 ./main.py --train_dqn --dqn_model=/tmp/rl/123.h5 --dqn_epsilon=0.5 --dqn_double_dqn #--dqn_dueling
