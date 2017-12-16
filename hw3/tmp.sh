f6eb27786260083b2cd391d2c574dc91d55edaf0
k123321141 <kk123321141@gamil.com>
2017-12-16 14:28:02 +0800

    quickly

--- hw3/tmp.sh
+++ hw3/tmp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-python3 ./main.py --train_pg \
-    --pg_model=./rl/pong_pg.h5 \
-    --pg_summary=./rl/summary/pong_pg \
+python3 ./main.py --train_dqn --dqn_dueling \
+    --dqn_model=./rl/DQN_duel \
+    --dqn_summary=./rl/summary/DQN_duel \
+
 #CUDA_VISIBLE_DEVICES="" python3 ./main.py --train_dqn --dqn_model=/tmp/rl/123.h5 --dqn_epsilon=0.5 --dqn_double_dqn #--dqn_dueling
 #CUDA_VISIBLE_DEVICES="" python3 ./main.py --train_dqn --dqn_model=/tmp/rl/123.h5 --dqn_epsilon=0.5 --dqn_double_dqn #--dqn_dueling
