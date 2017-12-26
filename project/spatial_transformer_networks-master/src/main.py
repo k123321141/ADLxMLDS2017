c37d2c7d2991a7502a42cd30a08e5d109c9d9bc9
k123321141 <kk123321141@gamil.com>
2017-12-27 03:02:16 +0800

    quickly

--- project/spatial_transformer_networks-master/src/main.py
+++ project/spatial_transformer_networks-master/src/main.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 import numpy as np
+import argparse
 np.random.seed(1337)  # for reproducibility
 from scipy.misc import imresize
 from keras.datasets import mnist
@@ -16,38 +17,56 @@ from keras.optimizers import Adam, SGD
 import keras.backend as K
 from spatial_transformer import SpatialTransformer
 
-batch_size = 128
+def parse():
+    parser = argparse.ArgumentParser(description="MLDS&ADL HW3")
+    parser.add_argument('--env_name', default=None, help='environment name')
+    parser.add_argument('--train_pg', action='store_true', help='whether train policy gradient')
+    parser.add_argument('--train_dqn', action='store_true', help='whether train DQN')
+    parser.add_argument('--test_pg', action='store_true', help='whether test policy gradient')
+    parser.add_argument('--test_dqn', action='store_true', help='whether test DQN')
+    parser.add_argument('--video_dir', default=None, help='output video directory')
+    parser.add_argument('--do_render', action='store_true', help='whether render environment')
+    try:
+        from argument import add_arguments
+        parser = add_arguments(parser)
+    except:
+        pass
+    args = parser.parse_args()
+    return args
+def main():
 
-DIM = 60
-dep = 3
-nb_classes = 13
-mnist_cluttered = '../datasets/train.npz'
+    batch_size = 128
+
+    DIM = 60
+    dep = 3
+    nb_classes = 13
+    mnist_cluttered = '../datasets/train.npz'
 
 
 # In[2]:
 
 
-data = np.load(mnist_cluttered)
-X_train, y_train = data['x_train'], data['y_train']
-X_valid, y_valid = data['x_valid'], data['y_valid']
-X_test, y_test = data['x_test'], data['y_test']
+    data = np.load(mnist_cluttered)
+    X_train, y_train = data['x_train'], data['y_train']
+    X_valid, y_valid = data['x_valid'], data['y_valid']
+    X_test, y_test = data['x_test'], data['y_test']
 # reshape for convolutions
-X_train = X_train.reshape((X_train.shape[0], DIM, DIM, dep))
-X_valid = X_valid.reshape((X_valid.shape[0], DIM, DIM, dep))
-X_test = X_test.reshape((X_test.shape[0], DIM, DIM, dep))
-print y_valid.shape
-y_train = np_utils.to_categorical(y_train, nb_classes)
-y_valid = np_utils.to_categorical(y_valid, nb_classes)
-y_test = np_utils.to_categorical(y_test, nb_classes)
-print y_valid.shape
-print("Train samples: {}".format(X_train.shape))
-print("Validation samples: {}".format(X_valid.shape))
-print("Test samples: {}".format(X_test.shape))
+    X_train = X_train.reshape((X_train.shape[0], DIM, DIM, dep))
+    X_valid = X_valid.reshape((X_valid.shape[0], DIM, DIM, dep))
+    X_test = X_test.reshape((X_test.shape[0], DIM, DIM, dep))
+    print y_valid.shape
+    y_train = np_utils.to_categorical(y_train, nb_classes)
+    y_valid = np_utils.to_categorical(y_valid, nb_classes)
+    y_test = np_utils.to_categorical(y_test, nb_classes)
+    print y_valid.shape
+    print("Train samples: {}".format(X_train.shape))
+    print("Validation samples: {}".format(X_valid.shape))
+    print("Test samples: {}".format(X_test.shape))
 
 
-input_shape =  np.squeeze(X_train.shape[1:])
-input_shape = (DIM, DIM, dep)
-print("Input shape:",input_shape)
+    input_shape =  np.squeeze(X_train.shape[1:])
+    input_shape = (DIM, DIM, dep)
+    print("Input shape:",input_shape)
 
 
 
@@ -58,101 +77,76 @@ print("Input shape:",input_shape)
 
 
 # initial weights
-b = np.zeros((2, 3), dtype='float32')
-b[0, 0] = 1
-b[1, 1] = 1
-W = np.zeros((50, 6), dtype='float32')
-weights = [W, b.flatten()]
+    b = np.zeros((2, 3), dtype='float32')
+    b[0, 0] = 1
+    b[1, 1] = 1
+    W = np.zeros((50, 6), dtype='float32')
+    weights = [W, b.flatten()]
 
 
 # In[5]:
 
 
-locnet = Sequential()
-locnet.add(MaxPooling2D(pool_size=(2,2), input_shape=input_shape))
-locnet.add(Convolution2D(20, (5, 5)))
-locnet.add(MaxPooling2D(pool_size=(2,2)))
-locnet.add(Convolution2D(20, (5, 5)))
+    locnet = Sequential()
+    locnet.add(MaxPooling2D(pool_size=(2,2), input_shape=input_shape))
+    locnet.add(Convolution2D(20, (5, 5)))
+    locnet.add(MaxPooling2D(pool_size=(2,2)))
+    locnet.add(Convolution2D(20, (5, 5)))
 
-locnet.add(Flatten())
-locnet.add(Dense(50))
-locnet.add(Activation('relu'))
-locnet.add(Dense(6, weights=weights ,name='trans_mat'))
+    locnet.add(Flatten())
+    locnet.add(Dense(50))
+    locnet.add(Activation('relu'))
+    locnet.add(Dense(6, weights=weights ,name='trans_mat'))
 #locnet.add(Activation('sigmoid'))
 
 
 # In[6]:
 
 
-model = Sequential()
+    model = Sequential()
 
-model.add(SpatialTransformer(localization_net=locnet,
-                             output_size=(DIM/2,DIM/2), input_shape=input_shape))
+    model.add(SpatialTransformer(localization_net=locnet,
+                                 output_size=(DIM/2,DIM/2), input_shape=input_shape))
 
-model.add(Convolution2D(32, (3, 3), padding='same'))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Convolution2D(32, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Convolution2D(32, (3, 3), padding='same'))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Convolution2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Flatten())
-model.add(Dense(256))
-model.add(Activation('relu'))
+    model.add(Flatten())
+    model.add(Dense(256))
+    model.add(Activation('relu'))
 
-model.add(Dense(nb_classes))
-model.add(Activation('softmax'))
-opt = Adam(lr=0.0001)
-model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer=opt)
-trans_lay = model.layers[0]
+    model.add(Dense(nb_classes))
+    model.add(Activation('softmax'))
+    opt = Adam(lr=0.0001)
+    model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer=opt)
+    trans_lay = model.layers[0]
 
-def show_trans_lay(lay):
-    k = lay.get_weights()[6]
-    b = lay.get_weights()[7]
-    print k
-    print b
-# show_trans_lay(trans_lay)
 
 
 # In[12]:
 
 
-XX = model.input
+    XX = model.input
 #model.load_weights('./model.h5')
-YY = model.layers[0].output
-F = K.function([XX], [YY])
-labels = ['GreenStraightRight', 'off', 'GreenStraightLeft', 'GreenStraight', 'RedStraightLeft', 'GreenRight', 'Yellow', 'RedStraight', 'Green', 'GreenLeft', 'RedRight', 'RedLeft', 'Red']
+    YY = model.layers[0].output
+    F = K.function([XX], [YY])
+    labels = ['GreenStraightRight', 'off', 'GreenStraightLeft', 'GreenStraight', 'RedStraightLeft', 'GreenRight', 'Yellow', 'RedStraight', 'Green', 'GreenLeft', 'RedRight', 'RedLeft', 'Red']
 #summary
-def setup_summary(labels):
-    summary_vars = [tf.Variable(0.) for i in range(len(labels) *5 +1 )] 
-    summary_placeholders = [tf.Variable(0.) for var in summary_vars ]
-    
-    tf.summary.scalar('Total_Accuracy/Epoch', summary_vars[0])
-        
-    for i,label in enumerate(labels): 
-        idx = 5*i + 1
-        tf.summary.scalar('%s_%s/Epoch' % (label, 'Accuracy'), summary_vars[idx])
-        tf.summary.scalar('%s_%s/Epoch' % (label, 'True_Positive_Rate'), summary_vars[idx+1])
-        tf.summary.scalar('%s_%s/Epoch' % (label, 'True_Positive'), summary_vars[idx+2])
-        tf.summary.scalar('%s_%s/Epoch' % (label, 'False_Positive'), summary_vars[idx+3])
-        tf.summary.scalar('%s_%s/Epoch' % (label, 'False_Negative'), summary_vars[idx+4])
-    
-    
-    update_ops = [summary_vars[i].assign(summary_placeholders[i]) for i in
-                  range(len(summary_vars))]
-    summary_op = tf.summary.merge_all()
-    return summary_placeholders, update_ops, summary_op
-sess = tf.InteractiveSession()
-K.set_session(sess)
+    sess = tf.InteractiveSession()
+    K.set_session(sess)
 
 
 
-summary_placeholders, update_ops, summary_op =             setup_summary(labels)
-summary_writer = tf.summary.FileWriter(
-            './summary'  , sess.graph)
-sess.run(tf.global_variables_initializer())
-        
-        
+    summary_placeholders, update_ops, summary_op =             setup_summary(labels)
+    summary_writer = tf.summary.FileWriter(
+                './summary'  , sess.graph)
+    sess.run(tf.global_variables_initializer())
+            
+            
 
 
 
@@ -160,6 +154,118 @@ sess.run(tf.global_variables_initializer())
 
 
 
+        
+    tb_callback = LambdaCallback(
+        on_epoch_end=lambda epoch, logs: class_acc(model, X_test, y_test, labels, epoch)
+    )
+
+
+        
+        
+        
+        
+
+
+
+
+    nb_epochs = 100 # you probably want to go longer than this
+    batch_size = 256
+
+        
+        
+    trans_lay = model.layers[0]
+
+    reset_callback = LambdaCallback(
+        on_batch_end=lambda batch, logs: reset_trans_lay(trans_lay)
+    )
+
+
+
+    try:
+        
+        for e in range(nb_epochs): 
+            print('-'*40)
+            #progbar = generic_utils.Progbar(X_train.shape[0])
+#         for b in range( ceil(float(X_train.shape[0]) / batch_size) ):
+#             print('%7d / %7d' % (f, X_train.shape[0]))
+
+#             f = b * batch_size
+#             l = min(X_train.shape[0]+1, (b+1) * batch_size)
+#             X_batch = X_train[f:l]
+#             y_batch = y_train[f:l]
+                
+                
+#             loss = model.train_on_batch(X_batch, y_batch)
+#             reset_trans_lay(trans_lay)
+            
+                
+#             #print(loss)
+#             #progbar.add(X_batch.shape[0], values=[("train loss", loss)])
+#         scorev = model.evaluate(X_valid, y_valid, verbose=1)
+#         scoret = model.evaluate(X_test, y_test, verbose=1)
+            model.fit(X_train, y_train, epochs=100, batch_size=batch_size, validation_data=(X_test, y_test),
+                      callbacks=[reset_callback, tb_callback])
+        #         print('Epoch: {0} | Valid: {1} | Test: {2}'.format(e, scorev, scoret))
+        
+            
+    except KeyboardInterrupt:
+        #confusion matrix
+        confusion_matrix(model, X_test, y_test, labels)
+
+def confusion_matrix(model,x, y_true, labels):
+    y_pred = np.argmax(model.predict(x), axis=-1)
+    y_true = np.argmax(y_true, axis=-1)
+    num = y_true.shape[0]
+    #count labels
+    count = [0] * len(labels)
+    #True Positive
+    tp = [0.] * len(labels)
+    #True Negative
+    tn = [0.] * len(labels)
+    #False Positive
+    fp = [0.] * len(labels)
+    #False Negative
+    fn = [0.] * len(labels)
+    
+    for i in range(num):
+        y_p,y_t, = y_pred[i], y_true[i]
+        count[y_t] += 1
+        if y_p == y_t:
+            tp[y_t] += 1
+            for i in range(len(labels)):
+                if i != y_t:
+                    fn[i] += 1
+        else:
+            tn[y_t] += 1
+            fp[y_p] += 1     
+    for i,label in enumerate(labels):
+        print 'label : %10s' % label
+        print '-'*20
+        print '%5d  ,  %5d' % (tp[i], tn[i])
+        print '%5d  ,  %5d' % (fp[i], fn[i])
+        print '-'*20
+        print ''
+
+def ceil(x):
+    a = int(x)
+    return a+1 if x > a else a
+        
+def show_trans_lay(lay):
+    idx  = [0,2,4,5]
+    idx = range(6)
+    k = lay.get_weights()[6]
+    b = lay.get_weights()[7]
+    print k[:,idx]
+    print b[idx]
+def reset_trans_lay(lay):
+    ws = lay.get_weights()
+    k = ws[6]
+    b = ws[7]
+    k[:,1] = 0
+    k[:,3] = 0
+    b[1] = 0
+    b[3] = 0
+    lay.set_weights(ws)
 def class_acc(model,x, y_true, labels, epoch):
     y_pred = np.argmax(model.predict(x), axis=-1)
     y_true = np.argmax(y_true, axis=-1)
@@ -219,115 +325,23 @@ def class_acc(model,x, y_true, labels, epoch):
         })
     summary_str = sess.run(summary_op)
     summary_writer.add_summary(summary_str, epoch)
+
+def setup_summary(labels):
+    summary_vars = [tf.Variable(0.) for i in range(len(labels) *5 +1 )] 
+    summary_placeholders = [tf.Variable(0.) for var in summary_vars ]
     
-tb_callback = LambdaCallback(
-    on_epoch_end=lambda epoch, logs: class_acc(model, X_test, y_test, labels, epoch)
-)
-
-
-    
-    
-    
-    
-
-
-# In[14]:
-
-
-def ceil(x):
-    a = int(x)
-    return a+1 if x > a else a
-nb_epochs = 100 # you probably want to go longer than this
-batch_size = 256
-
-def show_trans_lay(lay):
-    idx  = [0,2,4,5]
-    idx = range(6)
-    k = lay.get_weights()[6]
-    b = lay.get_weights()[7]
-    print k[:,idx]
-    print b[idx]
-def reset_trans_lay(lay):
-    ws = lay.get_weights()
-    k = ws[6]
-    b = ws[7]
-    k[:,1] = 0
-    k[:,3] = 0
-    b[1] = 0
-    b[3] = 0
-    lay.set_weights(ws)
-    
-    
-trans_lay = model.layers[0]
-
-reset_callback = LambdaCallback(
-    on_batch_end=lambda batch, logs: reset_trans_lay(trans_lay)
-)
-
-
-
-try:
-    
-    for e in range(nb_epochs): 
-        print('-'*40)
-        #progbar = generic_utils.Progbar(X_train.shape[0])
-#         for b in range( ceil(float(X_train.shape[0]) / batch_size) ):
-#             print('%7d / %7d' % (f, X_train.shape[0]))
-
-#             f = b * batch_size
-#             l = min(X_train.shape[0]+1, (b+1) * batch_size)
-#             X_batch = X_train[f:l]
-#             y_batch = y_train[f:l]
-            
-            
-#             loss = model.train_on_batch(X_batch, y_batch)
-#             reset_trans_lay(trans_lay)
+    tf.summary.scalar('Total_Accuracy/Epoch', summary_vars[0])
         
-            
-#             #print(loss)
-#             #progbar.add(X_batch.shape[0], values=[("train loss", loss)])
-#         scorev = model.evaluate(X_valid, y_valid, verbose=1)
-#         scoret = model.evaluate(X_test, y_test, verbose=1)
-        model.fit(X_train, y_train, epochs=100, batch_size=batch_size, validation_data=(X_test, y_test),
-                  callbacks=[reset_callback, tb_callback])
-    #         print('Epoch: {0} | Valid: {1} | Test: {2}'.format(e, scorev, scoret))
-        
-            
-except KeyboardInterrupt:
-    #confusion matrix
-    def confusion_matrix(model,x, y_true, labels):
-        y_pred = np.argmax(model.predict(x), axis=-1)
-        y_true = np.argmax(y_true, axis=-1)
-        num = y_true.shape[0]
-        #count labels
-        count = [0] * len(labels)
-        #True Positive
-        tp = [0.] * len(labels)
-        #True Negative
-        tn = [0.] * len(labels)
-        #False Positive
-        fp = [0.] * len(labels)
-        #False Negative
-        fn = [0.] * len(labels)
-        
-        for i in range(num):
-            y_p,y_t, = y_pred[i], y_true[i]
-            count[y_t] += 1
-            if y_p == y_t:
-                tp[y_t] += 1
-                for i in range(len(labels)):
-                    if i != y_t:
-                        fn[i] += 1
-            else:
-                tn[y_t] += 1
-                fp[y_p] += 1     
-        for i,label in enumerate(labels):
-            print 'label : %10s' % label
-            print '-'*20
-            print '%5d  ,  %5d' % (tp[i], tn[i])
-            print '%5d  ,  %5d' % (fp[i], fn[i])
-            print '-'*20
-            print ''
-    confusion_matrix(model, X_test, y_test, labels)
-
-        
+    for i,label in enumerate(labels): 
+        idx = 5*i + 1
+        tf.summary.scalar('%s_%s/Epoch' % (label, 'Accuracy'), summary_vars[idx])
+        tf.summary.scalar('%s_%s/Epoch' % (label, 'True_Positive_Rate'), summary_vars[idx+1])
+        tf.summary.scalar('%s_%s/Epoch' % (label, 'True_Positive'), summary_vars[idx+2])
+        tf.summary.scalar('%s_%s/Epoch' % (label, 'False_Positive'), summary_vars[idx+3])
+        tf.summary.scalar('%s_%s/Epoch' % (label, 'False_Negative'), summary_vars[idx+4])
+    
+    
+    update_ops = [summary_vars[i].assign(summary_placeholders[i]) for i in
+                  range(len(summary_vars))]
+    summary_op = tf.summary.merge_all()
+    return summary_placeholders, update_ops, summary_op
