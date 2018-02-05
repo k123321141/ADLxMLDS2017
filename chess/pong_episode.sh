236c17c5c2271e1d84a67831cf5a8a08b094b371
k123321141 <kk123321141@gamil.com>
2018-02-05 17:34:16 +0800

    quickly

--- chess/pong_episode.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-CUDA_VISIBLE_DEVICES="" python3 ./main.py --train_pg \
-    --pg_model=./rl/pong_pg_episode.h5 \
-    --pg_summary=./rl/summary/pong_pg_episode_summary 
-#CUDA_VISIBLE_DEVICES="" python3 ./main.py --train_dqn --dqn_model=/tmp/rl/123.h5 --dqn_epsilon=0.5 --dqn_double_dqn #--dqn_dueling
-#CUDA_VISIBLE_DEVICES="" python3 ./main.py --train_dqn --dqn_model=/tmp/rl/123.h5 --dqn_epsilon=0.5 --dqn_double_dqn #--dqn_dueling
