41696382bcd71b304e148c8ddf51b37b5f86574c
k123321141 <kk123321141@gamil.com>
2018-02-06 17:59:58 +0800

    quickly

--- chess/pong.sh
+++ chess/pong.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rm ./models/pong_pg.h5
-python3 ./main.py --train_pg \
+#rm ./models/pong_pg.h5
+python3 -u ./main.py --train_pg \
     --pg_model=./models/pong_pg.h5 \
     --pg_summary=./summary/pong_pg_summary_keep
 #CUDA_VISIBLE_DEVICES="" python3 ./main.py --train_dqn --dqn_model=/tmp/rl/123.h5 --dqn_epsilon=0.5 --dqn_double_dqn #--dqn_dueling
