64309693dc2b42dae57d3d263529c26810cc647a
k123321141 <kk123321141@gamil.com>
2018-03-08 10:19:26 +0800

    add rl

---
+++ chess/rl_a3c_pytorch/my_render.py
@@ -0,0 +1,24 @@
+
+import numpy as np
+class my_render():
+    def __init__(self):
+        from gym.envs.classic_control import rendering
+        self.viewer = rendering.SimpleImageViewer()
+    def render(self, rgb):
+        #rgb render
+        #upscaled=self.repeat_upsample(rgb,3, 3)
+        #self.viewer.imshow(upscaled)
+        self.viewer.imshow(rgb)
+
+    def repeat_upsample(self, rgb_array, k=1, l=1, err=[]):
+        # repeat kinda crashes if k/l are zero
+        if k <= 0 or l <= 0: 
+            if not err: 
+                print("Number of repeats must be larger than 0, k: {}, l: {}, returning default array!".format(k, l))
+                err.append('logged')
+            return rgb_array
+
+        # repeat the pixels k times along the y axis and l times along the x axis
+        # if the input image is of shape (m,n,3), the output image will be of shape (k*m, l*n, 3)
+
+        return np.repeat(np.repeat(rgb_array, k, axis=0), l, axis=1)
---
+++ chess/rl_a3c_pytorch/tmp.sh
@@ -0,0 +1,2 @@
+python -u main.py --env PongDeterministic-v4 --workers 1
+#python -u main.py --env Pong-ramDeterministic-v4 --workers 1
