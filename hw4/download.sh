8da14d87b37edddb15aa07270aa0b9f8bf4934da
k123321141 <kk123321141@gmail.com>
2018-04-02 16:07:49 +0800

    update download script

--- hw4/download.sh
+++ hw4/download.sh
@@ -5,11 +5,13 @@ mkdir -p ${PRETRAINED_MODELS_DIR}
 cd ${PRETRAINED_MODELS_DIR}
 
 # Download and extract the unidirectional model.
-wget "http://download.tensorflow.org/models/skip_thoughts_uni_2017_02_02.tar.gz"
+wget -q "http://download.tensorflow.org/models/skip_thoughts_uni_2017_02_02.tar.gz"
 tar -xvf skip_thoughts_uni_2017_02_02.tar.gz
 rm skip_thoughts_uni_2017_02_02.tar.gz
+echo 'done 1'
 
 # Download and extract the bidirectional model.
-wget "http://download.tensorflow.org/models/skip_thoughts_bi_2017_02_16.tar.gz"
+wget -q "http://download.tensorflow.org/models/skip_thoughts_bi_2017_02_16.tar.gz"
 tar -xvf skip_thoughts_bi_2017_02_16.tar.gz
 rm skip_thoughts_bi_2017_02_16.tar.gz
+echo 'done'
